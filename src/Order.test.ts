13fa90a652089aabe868028b8639412c7d50d12c
Simon Cragg <simoncragg@icloud.com>
2022-02-20 15:54:08 +0000

    feat: total an order with no items

--- src/Order.test.ts
+++ src/Order.test.ts
@@ -1,5 +1,8 @@
+import { Order } from "./Order";
+
 describe("Order", () => {
-  it("should pass", () => {
-    expect(true).toBeTruthy();
+  it("should return 0 given no items", () => {
+    const order = new Order();
+    expect(order.totalPence).toBe(0);
   });
 });
---
+++ src/Order.ts
@@ -0,0 +1,5 @@
+export class Order {
+  get totalPence(): number {
+    return 0;
+  }
+}
